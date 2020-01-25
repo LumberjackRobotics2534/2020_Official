c0d5f8dbc6f1f1c674d815450d46ac3941c51c12
unknown <bl@USROBOTICS-01.boyslatinmd.com>
2020-01-25 15:42:43 -0500

    MotionProfiling

--- src/main/java/frc/robot/commands/RotationControl.java
+++ src/main/java/frc/robot/commands/RotationControl.java
@@ -8,7 +8,6 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import frc.robot.RobotContainer;
 import frc.robot.subsystems.ColorWheel;
 public class RotationControl extends CommandBase {
   /**
--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -8,10 +8,17 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.NeutralMode;
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.kauailabs.navx.frc.AHRS;
 
+import edu.wpi.first.wpilibj.SerialPort;
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.drive.MecanumDrive;
+import edu.wpi.first.wpilibj.geometry.Pose2d;
+import edu.wpi.first.wpilibj.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.kinematics.DifferentialDriveOdometry;
+import edu.wpi.first.wpilibj.kinematics.DifferentialDriveWheelSpeeds;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
@@ -20,8 +27,14 @@ public class DriveTrain extends SubsystemBase {
   private WPI_TalonSRX rightBack = new WPI_TalonSRX(Constants.rightBackDrive);
   private WPI_TalonSRX leftFront = new WPI_TalonSRX(Constants.leftFrontDrive);
   private WPI_TalonSRX leftBack = new WPI_TalonSRX(Constants.leftBackDrive);
+  private AHRS navx = new AHRS(SerialPort.Port.kMXP);
 
+  
   private MecanumDrive mecanumDriveTrain = new MecanumDrive(leftFront, leftBack, rightFront, rightBack);
+  private SpeedControllerGroup left = new SpeedControllerGroup(leftFront, leftBack);
+  private SpeedControllerGroup right = new SpeedControllerGroup(rightFront, rightBack);
+  private DifferentialDrive differentialDriveTrain = new DifferentialDrive(left, right);
+  private DifferentialDriveOdometry m_odometry;
 
   public DriveTrain() {
     rightFront.configFactoryDefault();
@@ -39,12 +52,27 @@ public class DriveTrain extends SubsystemBase {
     leftFront.setInverted(false);
     leftBack.setInverted(false);
   }
+  public double getHeading() {
+    return Math.IEEEremainder(navx.getAngle(), 360) * (1.0);
+  }
   public void drive(double _ySpeed, double _xSpeed, double _rot){
     mecanumDriveTrain.driveCartesian(_ySpeed, _xSpeed, _rot);
   }
-
+  public void tankDriveVolts(double leftVolts, double rightVolts){
+    left.setVoltage(leftVolts);
+    right.setVoltage(rightVolts);
+    differentialDriveTrain.feed();
+  }
   @Override
   public void periodic() {
-    // This method will be called once per scheduler run
+    
+    m_odometry.update(Rotation2d.fromDegrees(getHeading()), leftFront.getSelectedSensorPosition(), rightFront.getSelectedSensorPosition());
+  }
+  public Pose2d getPose() {
+    return m_odometry.getPoseMeters();
+    
+  }
+  public DifferentialDriveWheelSpeeds getWheelSpeeds() {
+    return new DifferentialDriveWheelSpeeds(leftFront.getSelectedSensorVelocity(), rightFront.getSelectedSensorVelocity());
   }
 }
