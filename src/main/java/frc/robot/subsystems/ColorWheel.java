a0e17cbd4e96d8f0e274f47c51daefd0690b189d
lhazard22 <lhazard22@boyslatinmd.com>
2020-02-11 16:50:13 -0500

    Added Color Wheel code - is still buggy

--- src/main/java/frc/robot/subsystems/ColorWheel.java
+++ src/main/java/frc/robot/subsystems/ColorWheel.java
@@ -9,14 +9,29 @@ package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.revrobotics.ColorMatch;
+import com.revrobotics.ColorMatchResult;
+import com.revrobotics.ColorSensorV3;
+
+import edu.wpi.first.wpilibj.I2C;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 
 public class ColorWheel extends SubsystemBase {
-  public static WPI_TalonSRX colorWheelMotor = new WPI_TalonSRX(Constants.colorWheelMotor);
-  /*private final I2C.Port i2cPort = I2C.Port.kOnboard;
-  ColorSensorV3 colorSensor = new ColorSensorV3(i2cPort);*/
+  private static WPI_TalonSRX colorWheelMotor = new WPI_TalonSRX(Constants.colorWheelMotor);
+  private final I2C.Port i2cPort = I2C.Port.kOnboard;
+  private final ColorSensorV3 colorSensor = new ColorSensorV3(i2cPort);
+  private final ColorMatch m_colorMatcher = new ColorMatch();
+  private final Color kBlueTarget = ColorMatch.makeColor(0.143, 0.427, 0.429);
+  private final Color kGreenTarget = ColorMatch.makeColor(0.197, 0.561, 0.240);
+  private final Color kRedTarget = ColorMatch.makeColor(0.561, 0.232, 0.114);
+  private final Color kYellowTarget = ColorMatch.makeColor(0.361, 0.524, 0.113);
+  private Color detectedColor;
+  private String colorString;
+  private ColorMatchResult match;
   public ColorWheel() {
 
     colorWheelMotor.configFactoryDefault();
@@ -24,6 +39,11 @@ public class ColorWheel extends SubsystemBase {
     colorWheelMotor.setNeutralMode(NeutralMode.Brake);
 
     colorWheelMotor.setInverted(false);
+
+    m_colorMatcher.addColorMatch(kBlueTarget);
+    m_colorMatcher.addColorMatch(kGreenTarget);
+    m_colorMatcher.addColorMatch(kRedTarget);
+    m_colorMatcher.addColorMatch(kYellowTarget);
   }
 
   public void spinColorWheel(double _speed) {
@@ -34,6 +54,21 @@ public class ColorWheel extends SubsystemBase {
 
   @Override
   public void periodic() {
-    // This method will be called once per scheduler run
+    detectedColor = colorSensor.getColor();
+    match = m_colorMatcher.matchClosestColor(detectedColor);
+
+    if (match.color == kBlueTarget) {
+      colorString = "Blue";
+    } else if (match.color == kRedTarget) {
+      colorString = "Red";
+    } else if (match.color == kGreenTarget) {
+      colorString = "Green";
+    } else if (match.color == kYellowTarget) {
+      colorString = "Yellow";
+    } else {
+      colorString = "Unknown";
+    }
+    SmartDashboard.putNumber("Confidence", match.confidence);
+    SmartDashboard.putString("Detected Color", colorString);
   }
 }
